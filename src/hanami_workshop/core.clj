44aec5675aebc42e43ddef18eec41587975fc2d8
mabrams <mabrams@uab.edu>
2021-11-19 17:53:03 -0600

    initial commit--mostly 'lein new' files

---
+++ src/hanami_workshop/core.clj
@@ -0,0 +1,6 @@
+(ns hanami-workshop.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/hanami_workshop/core_test.clj
@@ -0,0 +1,7 @@
+(ns hanami-workshop.core-test
+  (:require [clojure.test :refer :all]
+            [hanami-workshop.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
